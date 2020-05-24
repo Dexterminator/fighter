0ade7bc3c30e57b3e01beb5c7bc0a986eb99cc7e
Dexter Gramfors <dexter.gramfors@trioptima.com>
2020-05-24 19:30:19 -0400

    Networking

--- src/index.js
+++ src/index.js
@@ -1,55 +1,95 @@
 import "./styles.css"
-import {initCanvas, mainConstants, networkSendInputs, render, update} from "./main"
-import {initGuestPeer, initHostPeer, testPeer} from "./network"
+import {initCanvas, mainConstants, render, update} from "./main"
+import {encodeInput, initGuestPeer, initHostPeer, parseRemoteInput, resolveNetworking, testPeer} from "./network"
 
-(function () {
+export function networkSendInputs(peer, inputsByFrame) {
+  const json = encodeInput(inputsByFrame)
+  peer.send(json)
+}
+
+export function startGame(peer, playerId) {
+  let networkState = {
+    latestSyncedFrame: -1,
+    remoteInputByFrame: {}
+  }
+
+  peer.on('data', data => {
+    networkState.remoteInputByFrame = data
+    console.log('data: ' + data)
+  })
+
   const canvas = initCanvas()
   const ctx = canvas.getContext("2d")
   let state = JSON.parse(mainConstants.initialState)
   let frames = 0
-  const inputsByFrame = {}
-  const statesByFrame = {}
+  const localInputsByFrame = {}
+  const statesByFrame = {'-1': mainConstants.initialState}
   const debugConfig = {savedState: mainConstants.initialState}
-  const remoteStatesByFrame = {}
   for (let i = 0; i < mainConstants.FRAME_DELAY; i++) {
-    inputsByFrame[i] = new Set()
+    localInputsByFrame[i] = new Set()
   }
 
   const currentInputs = new Set()
-  const player2Inputs = new Set()
+  // const player2Inputs = new Set()
   window.addEventListener('keydown', function (e) {
     currentInputs.add(mainConstants.inputsByKey[e.code])
-    player2Inputs.add(mainConstants.inputsByKey2[e.code])
+    // player2Inputs.add(mainConstants.inputsByKey2[e.code])
     if (e.code === 'Space') {
       // initPeer()
     }
   })
   window.addEventListener('keyup', function (e) {
     currentInputs.delete(mainConstants.inputsByKey[e.code])
-    player2Inputs.delete(mainConstants.inputsByKey2[e.code])
+    // player2Inputs.delete(mainConstants.inputsByKey2[e.code])
   })
 
+  let hasWinner = false
   function main() {
     let stopMain = window.requestAnimationFrame(main)
-    inputsByFrame[frames + mainConstants.FRAME_DELAY] = new Set(currentInputs)
-    networkSendInputs(inputsByFrame)
-    const player1Inputs = inputsByFrame[frames] || new Set()
-    update(state, player1Inputs, player2Inputs)
+    localInputsByFrame[frames + mainConstants.FRAME_DELAY] = new Set(currentInputs)
+    console.log(frames)
+    networkSendInputs(peer, localInputsByFrame)
+    let [newLatestSyncedFrame, newInputsByFrame, newState] = resolveNetworking(localInputsByFrame, networkState.remoteInputByFrame, statesByFrame, networkState.latestSyncedFrame, frames)
+    state = newState
+    networkState.latestSyncedFrame = newLatestSyncedFrame
+    const localInputs = localInputsByFrame[frames]
+    if (playerId === 'player1') {
+      update(state, localInputs, newInputsByFrame[frames])
+    } else {
+      update(state, newInputsByFrame[frames], localInputs)
+    }
+
     statesByFrame[frames] = JSON.stringify(state)
     render(state, ctx, canvas)
     frames++
-    delete inputsByFrame[frames - 10]
+    delete localInputsByFrame[frames - 10]
     delete statesByFrame[frames - 10]
-    if (state.player1.hp <= 0 || state.player2.hp <= 0) {
-      frames = 0
-      state = JSON.parse(mainConstants.initialState)
-      // remoteInput = JSON.stringify({})
-      // networkState = {latestSyncedFrame: -1}
+    if (state.player1.hp <= 0 && !hasWinner) {
+      const h1 = document.createElement('h1')
+      h1.textContent = 'Player 2 wins!'
+      document.body.append(h1)
+      hasWinner = true
     }
+
+    if (state.player2.hp <= 0 && !hasWinner) {
+      const h1 = document.createElement('h1')
+      h1.textContent = 'Player 1 wins!'
+      document.body.append(h1)
+      hasWinner = true
+    }
+
+    // if (state.player1.hp <= 0 || state.player2.hp <= 0) {
+    //   frames = 0
+    //   state = JSON.parse(mainConstants.initialState)
+    //   networkState.remoteInputByFrame = {}
+    //   networkState.latestSyncedFrame = -1
+    // }
   }
 
   main()
+}
 
+(function () {
   if (location.hash === '#host') {
     initHostPeer()
   } else if (location.hash === '#guest') {
--- src/network.js
+++ src/network.js
@@ -1,6 +1,7 @@
 import SimplePeer from "simple-peer"
 import {MAX_PREDICTION_WINDOW} from "./constants"
 import {update} from "./main"
+import {startGame} from "./index"
 
 export function parseRemoteInput(remoteInput) {
   return JSON.parse(remoteInput, (key, value) => value instanceof Array ? new Set(value) : value)
@@ -10,7 +11,7 @@ export function encodeInput(input) {
   return JSON.stringify(input, (key, value) => value instanceof Set ? [...value] : value)
 }
 
-export function resolveNetworking(inputsByFrame, remoteInputsByFrame, statesByFrame, latestSyncedFrame, currentFrame) {
+export function resolveNetworking(inputsByFrame, remoteInputsByFrame, statesByFrame, latestSyncedFrame, currentFrame, playerId) {
   remoteInputsByFrame = remoteInputsByFrame || JSON.stringify({})
   remoteInputsByFrame = parseRemoteInput(remoteInputsByFrame)
   remoteInputsByFrame[-1] = new Set()
@@ -26,7 +27,11 @@ export function resolveNetworking(inputsByFrame, remoteInputsByFrame, statesByFr
 
   const state = JSON.parse(statesByFrame[latestSyncedFrame])
   for (let i = latestSyncedFrame + 1; i < currentFrame; i++) {
-    update(state, inputsByFrame[i], remoteInputsByFrame[i])
+    if (playerId === 'player1') {
+      update(state, inputsByFrame[i], remoteInputsByFrame[i])
+    } else {
+      update(state, remoteInputsByFrame[i], inputsByFrame[i])
+    }
     statesByFrame[i] = JSON.stringify(state)
   }
 
@@ -38,6 +43,10 @@ export function networkSendInputs(inputsByFrame) {
   const json = encodeInput(inputsByFrame)
 }
 
+function appendControls() {
+
+}
+
 export function initHostPeer() {
   const p = new SimplePeer({
     initiator: true,
@@ -45,24 +54,24 @@ export function initHostPeer() {
   })
   p.on('error', err => console.log('error', err))
 
+  const div = document.createElement('div')
+  const div2 = document.createElement('div')
+  const div3 = document.createElement('div')
+  const textArea = document.createElement('textarea')
+  const button = document.createElement("button")
   p.on('signal', data => {
     console.log('SIGNAL', JSON.stringify(data))
-    const div = document.createElement('div')
-    const div2 = document.createElement('div')
     div.textContent = "Paste the below in the guest's form!"
     div2.textContent = JSON.stringify(data)
     div2.style = 'color: teal'
     document.body.append(div)
     document.body.append(div2)
 
-    const div3 = document.createElement('div')
     div3.textContent = "Paste the guest's response here."
 
     document.body.append(document.createElement('br'))
     document.body.append(div3)
-    const textArea = document.createElement('textarea')
     document.body.append(textArea)
-    const button = document.createElement("button")
     button.textContent = 'Start game!'
     button.onclick = () => {
       p.signal(JSON.parse(textArea.value))
@@ -73,10 +82,12 @@ export function initHostPeer() {
   p.on('connect', () => {
     console.log('CONNECTED TO GUEST')
     p.send('whatever' + Math.random())
-  })
-
-  p.on('data', data => {
-    console.log('data: ' + data)
+    div.remove()
+    div2.remove()
+    div3.remove()
+    textArea.remove()
+    button.remove()
+    startGame(p, 'player1')
   })
 }
 
@@ -98,48 +109,27 @@ export function initGuestPeer() {
   }
   document.body.append(button)
 
+  const div2 = document.createElement('div')
+  const div3 = document.createElement('div')
+
   p.on('signal', data => {
     console.log('SIGNAL', JSON.stringify(data))
-    const div = document.createElement('div')
-    const div2 = document.createElement('div')
-    div.textContent = "Paste this in the host's form!"
-    div2.textContent = JSON.stringify(data)
-    div2.style = 'color: teal'
-    document.body.append(div)
-    document.body.append(document.createElement('br'))
+    div2.textContent = "Paste this in the host's form!"
+    div3.textContent = JSON.stringify(data)
+    div3.style = 'color: teal'
     document.body.append(div2)
+    document.body.append(document.createElement('br'))
+    document.body.append(div3)
   })
 
   p.on('connect', () => {
     console.log('CONNECTED TO HOST')
     p.send('whatever' + Math.random())
-  })
-
-  p.on('data', data => {
-    console.log('data: ' + data)
-  })
-}
-
-export function testPeer() {
-  const p = new SimplePeer({
-    initiator: location.hash === '#host',
-    trickle: false
-  })
-
-  const textArea = document.createElement('textarea')
-  document.body.append(textArea)
-
-  document.querySelector('form').addEventListener('submit', ev => {
-    ev.preventDefault()
-    p.signal(JSON.parse(document.querySelector('#incoming').value))
-  })
-
-  p.on('connect', () => {
-    console.log('CONNECT')
-    p.send('whatever' + Math.random())
-  })
-
-  p.on('data', data => {
-    console.log('data: ' + data)
+    div.remove()
+    div2.remove()
+    div3.remove()
+    textArea.remove()
+    button.remove()
+    startGame(p, 'player2')
   })
 }
